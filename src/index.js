404e7150869c3764c95c76c96da423b1eacded0f
Sho Iizuka <iizuka.sho.ij4@is.naist.jp>
2016-10-05 21:29:52 +0900

    Add material-ui dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+// import App from './App';
+// import './index.css';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+injectTapEventPlugin();
+
+const App = () => (
+  <MuiThemeProvider>
+    <RaisedButton label="Default" />
+  </MuiThemeProvider>
+);
 
 ReactDOM.render(
   <App />,
